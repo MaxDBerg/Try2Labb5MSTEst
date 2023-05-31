51ae699e59959e996a2b580e57f8857726771646
MaxDBerg <max.dahlberg@gmail.com>
2023-05-31 12:35:27 +0200

    Added mstest

---
+++ MS-TestStuff/LoanMoneyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Labb5MS_Test
+{
+    [TestClass]
+    internal class LoanMoneyTest
+    {
+
+    }
+}
---
+++ MS-TestStuff/OpenAccountTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labb5MS_Test
+{
+    [TestClass]
+    public class OpenAccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
